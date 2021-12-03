4cd19950b1e343005d4266a35f8b6188f21bbf55
yedidasubhas <94880150+yedidasubhas@users.noreply.github.com>
2021-12-03 16:24:08 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { NgModule, Component, enableProdMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
+
+import { DxTreeListModule } from 'devextreme-angular';
+
+if (!/localhost/.test(document.location.host)) {
+  enableProdMode();
+}
+
+@Component({
+  selector: 'demo-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  dataSource: any;
+
+  constructor(http: HttpClient) {
+    this.dataSource = {
+      load(loadOptions) {
+        let params = new HttpParams();
+        if (loadOptions.parentIds) {
+          loadOptions.parentIds.forEach((id) => {
+            params = params.append('parentIds', id);
+          });
+        }
+        return http
+          .get('https://js.devexpress.com/Demos/Mvc/api/treeListData', { params })
+          .toPromise();
+      },
+    };
+  }
+
+  customizeSizeText(e) {
+    if (e.value !== null) {
+      return `${Math.ceil(e.value / 1024)} KB`;
+    }
+  }
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    DxTreeListModule,
+    HttpClientModule,
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
